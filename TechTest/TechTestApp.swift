3b26fec5a0183e59793a190e506ea09bf6defbae
Kyle Thomas <kyle@kmtcomputing.com>
2024-10-30 15:06:27 +0000

    Initial Commit

---
+++ TechTest/TechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechTestApp.swift
+//  TechTest
+//
+//  Created by Kyle Thomas on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
